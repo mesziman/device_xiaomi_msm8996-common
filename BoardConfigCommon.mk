omnirom/android_device_xiaomi_msm8996-common

Don't build generic camera HAL. - Thanks for pointing out @DennySpb - Add android.hardware.camera.provider 2.4 service
Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

BoardConfigCommon.mk
@@ -104,6 +104,7 @@ QCOM_BT_USE_BTNV := true
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+DEVICE_SPECIFIC_CAMERA_PATH := $(DEVICE_PATH)/camera
 TARGET_SUPPORT_HAL1 := false
 TARGET_USES_MEDIA_EXTENSIONS := true
 USE_DEVICE_SPECIFIC_CAMERA := true

mesziman/device_xiaomi_msm8996-common

BoardConfigCommon.mk
@@ -100,6 +100,7 @@ QCOM_BT_USE_BTNV := true
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+DEVICE_SPECIFIC_CAMERA_PATH := $(VENDOR_PATH)/camera
 TARGET_SUPPORT_HAL1 := false
 TARGET_USES_MEDIA_EXTENSIONS := true
 USE_DEVICE_SPECIFIC_CAMERA := true